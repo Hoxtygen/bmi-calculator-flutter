3dc09410a266ad115ee35feda08c23619ee13cd2
Wasiu Idowu <hoxtygen@live.com>
2020-09-15 08:54:33 +0100

    Create a file to put constant variables

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const double kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF1d1e33);
+const kInactiveCardColour = Color(0xff111328);
+const KBottomContainerColour = Color(0xffeb1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8d8e98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
