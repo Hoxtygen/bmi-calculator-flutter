3306e72b0c0c044da89e14d276919a6028dfdc0c
Wasiu Idowu <hoxtygen@live.com>
2020-09-26 08:10:25 +0100

    Remove log messages

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -9,20 +9,16 @@ class CalculatorBrain {
 
   String calculateBMI() {
     double _bmi = weight / pow(height / 100, 2);
-    print(_bmi.toStringAsFixed(1));
     return _bmi.toStringAsFixed(1);
   }
 
   String getResult(String bmi) {
     _bmi = double.parse(bmi);
     if (_bmi >= 25) {
-      print(_bmi);
       return "Overweight";
     } else if (_bmi >= 18.5) {
-      print(_bmi);
       return "Normal";
     } else {
-      print(_bmi);
       return "Underweight";
     }
   }
@@ -30,13 +26,10 @@ class CalculatorBrain {
   String getInterpretation(String bmi) {
     _bmi = double.parse(bmi);
     if (_bmi >= 25) {
-      print(_bmi);
       return "You have a higher than normal body weight. Try to exercise more";
     } else if (_bmi >= 18.5) {
-      print(_bmi);
       return "You have a normal body weight. Good job!";
     } else {
-      print(_bmi);
       return "You have a lower than normal body weight. You need to eat more.";
     }
   }
